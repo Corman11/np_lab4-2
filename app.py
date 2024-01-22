db9bad1425c379dd5452660e14d2702f647c1424
Corman11 <156091016+Corman11@users.noreply.github.com>
2024-01-22 01:32:07 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ def subtract_numbers(a , b):
   return a - b
 def multiply_numbers(a,b):
   return a* b
-def divide_numbers(a, b):
+def divide_numbers(a, b): 
   if b== 0:
     raise ValueError("Dzielenie przez zero jest niedozwolone.")
   return a /b
