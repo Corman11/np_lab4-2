eb3f1d8d4cff9f233b253312de99a9f034d4cd42
Corman11 <156091016+Corman11@users.noreply.github.com>
2024-01-22 01:19:43 +0100

    Update app_zad2.py

--- app_zad2.py
+++ app_zad2.py
@@ -16,7 +16,7 @@ def divide_numbers(a, b):
     return a / b
 
 
-if  __name__ == "__main__":
+if __name__ == "__main__":
     num1 = 10
     num2 = 5
     print(f"Dodawanie: {add_numbers(num1, num2)}")
